1b73deebcc1a7ddb4bcc71d76b9ba692bca77308
Manuel Sales <manuel.sales@gmail.com>
2018-04-06 15:37:43 +0100

    use appendChild to add elements to infoDiv

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -14,7 +14,7 @@ func main() {
 	domain := flag.String("domain", "localhost", "Server domain")
 	port := flag.String("port", "3030", "Server port")
 	static := flag.String("static", "/home/gugu/1devel/go/src/bitbucket.org/thex/iotaBrowser/static", "Static file path") // NOTE adapt static path to your system location
-	iriUrl := flag.String("iriurl", "http://p101.iotaledger.net:14700", "Iota IRI server:port")                           // NOTE if not works try other from this testnet list.. "http://iota.nessys.es:14700", "https://testnet140.tangle.works:443", "http://p101.iotaledger.net:14700", "http://p103.iotaledger.net:14700"
+	iriUrl := flag.String("iriurl", "http://iota.nessys.es:14700", "Iota IRI server:port")                                // NOTE if not works try other from this testnet list.. "http://iota.nessys.es:14700", "https://testnet140.tangle.works:443", "http://p101.iotaledger.net:14700", "http://p103.iotaledger.net:14700"
 
 	//  set IRI server
 	iri.Init(iriUrl)
